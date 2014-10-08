8041e2f8a0917776e42fc027fb159c9954de7391
Hideki HAYASI <linxs@linxs.org>
2014-10-08 18:35:06 +0900

    Other Changes - Python 2.7 based code.

--- romaja.py
+++ romaja.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python2.7
 # vim: fileencoding=utf-8 fileformat=unix
 
 """romaja.py: Japanese Kana to Romaji converter
@@ -13,8 +13,10 @@ WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 FOR A PARTICULAR PURPOSE.
 """
 
+from __future__ import print_function
 
 import sys
+import os
 import re
 
 
@@ -33,12 +35,12 @@ if sys.platform.startswith("win"):
     ################## py2exe compliant ##################
 
 
-__version__ = "0.3.2"
+__version__ = "1.0.0"
 __author__ = "HAYASI Hideki"
 __copyright__ = "Copyright (C) 2013 HAYASI Hideki <linxs@linxs.org>"
 __license__ = "ZPL 2.1"
 __email__ = "linxs@linxs.org"
-__status__ = "Development"
+__status__ = "Production"
 
 __all__ = ("romazi", "romaji")
 
@@ -115,64 +117,56 @@ def romazi(s):
     return s
 
 
-def romaji(s, use_h=False, sup_apo=False):
+def romaji(s, use_h=False, suppress_apostrophe=False):
     s = romazi(s)
     s = re.sub(r"n([bmp])", r"m\1", s)
     s = _replace(s, "hu si zi ti tu sy zy ty", "fu shi ji chi tsu sh j ch")
     s = _replace(s, "i^ u^ e^", "ii u e")
     s = _replace(s, "a^ o^", "ah oh" if use_h else "a o")
-    if sup_apo:
+    if suppress_apostrophe:
         s = s.replace("'", "")
     return s
 
 
 def main():
 
-    from optparse import OptionParser
+    from argparse import ArgumentParser
     import codecs
 
-    parser = OptionParser(usage=u"""\
-    {python}{script} [options] [word_in_kana ...]
-
-    Convert Japanese words in kana into roman letters.
-    If no argument is given, convert words read from standard-input.""".format(
-        python="python " if sys.argv[0].endswith(".py") else "",
-        script=sys.argv[0]))
-    parser.add_option("--use_h",
-            dest="use_h", action="store_true",
-            help="represent long syllable with `h'")
-    parser.add_option("-A", "--suppress-apostrophe",
-            dest="sup_apo", action="store_true",
+    parser = ArgumentParser(description=u"""\
+            Convert Japanese words in kana into roman letters.
+            If no argument is given, convert words read from standard-input.
+            """)
+    arg = parser.add_argument
+    arg("--use-h", "--use_h", action="store_true", help="`h' as long syllable")
+    arg("-A", "--suppress-apostrophe", action="store_true",
             help="suppress apostrophe (') e.g. hon'ya -> honya")
-    parser.add_option("-k", "--kunrei",
-            dest="kunrei", action="store_true",
-            help="convert in kunrei-style")
-    parser.add_option("--encoding", dest="encoding",
-            help="set input-encoding")
-    parser.add_option("-v", "--version", dest="version", action="store_true",
-            help="show version information")
-    opts, args = parser.parse_args()
+    arg("-k", "--kunrei", action="store_true", help="convert in kunrei-style")
+    arg("--encoding", help="set input-encoding")
+    arg("-v", "--version", action="store_true", help="show version info")
+    arg("words", nargs="*", metavar="word")
+    args = parser.parse_args()
 
-    enc = opts.encoding or sys.stdin.encoding or \
+    enc = args.encoding or sys.stdin.encoding or \
             "cp932" if sys.platform.startswith("win") else "utf-8"
 
-    if opts.version:
-        cmd = sys.argv[0].decode(enc).split("\\")[-1].lower()
-        print "{cmd} version {version}".format(cmd=cmd, version=__version__)
-        print __copyright__
+    if args.version:
+        cmd = os.path.basename(sys.argv[0])
+        print("{cmd} version {ver}".format(cmd=cmd, ver=__version__))
+        print(__copyright__)
         sys.exit(0)
 
-    if opts.kunrei:
+    if args.kunrei:
         roman = romazi
     else:
-        roman = lambda s: romaji(s, use_h=opts.use_h, sup_apo=opts.sup_apo)
-    if args:
-        print " ".join(roman(arg.decode(enc)) for arg in args)
+        roman = lambda s: romaji(s, use_h=args.use_h,
+                        suppress_apostrophe=args.suppress_apostrophe)
+    if args.words:
+        print(u" ".join(roman(word.decode(enc)) for word in args.words))
     else:
         for line in codecs.getreader(enc)(sys.stdin):
-            print " ".join(roman(word) for word in line.split())
+            print(u" ".join(roman(word) for word in line.split()))
 
 
 if __name__ == "__main__":
-
     main()
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python2.6
+#/usr/bin/env python2.7
 # vim: fileencoding=utf-8 fileformat=unix
 
 from distutils.core import setup
